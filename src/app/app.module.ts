61fe495ef79685c87be6df0197fe68ee9e1124d8
Caleb Ooten <caleb.ooten@cgi.com>
2022-11-21 20:11:20 -0500

    Formatted app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 import { mockBackendProvider } from './helpers/mock-backend';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AssetService } from './services/asset.service';
-import { AssetList} from './assets/asset-list.component';
+import { AssetList } from './assets/asset-list.component';
 import { AssetDetails } from './assets/asset-details.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -21,10 +21,37 @@ import { LogService } from './shared/log.service';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule, MatTableModule, MatFormFieldModule, MatPaginatorModule, MatInputModule, ReactiveFormsModule, MatSortModule, MatSelectModule, MatSnackBarModule, MatProgressSpinnerModule, BrowserAnimationsModule],
-  declarations: [AppComponent, HelloComponent, AssetList, AssetDetails, NewAssetForm],
-  bootstrap: [AppComponent],
-  providers: [AssetService, LogService, mockBackendProvider]
+  imports: [
+    BrowserModule, 
+    FormsModule, 
+    HttpClientModule, 
+    AppRoutingModule, 
+    MatTableModule, 
+    MatFormFieldModule, 
+    MatPaginatorModule, 
+    MatInputModule, 
+    ReactiveFormsModule, 
+    MatSortModule, 
+    MatSelectModule, 
+    MatSnackBarModule, 
+    MatProgressSpinnerModule, 
+    BrowserAnimationsModule
+  ],
+  declarations: [
+    AppComponent, 
+    HelloComponent, 
+    AssetList, 
+    AssetDetails, 
+    NewAssetForm
+  ],
+  bootstrap: [
+    AppComponent
+  ],
+  providers: [
+    AssetService, 
+    LogService, 
+    mockBackendProvider
+  ]
 })
 
 export class AppModule { }
